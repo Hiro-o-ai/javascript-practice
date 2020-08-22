b62918b596f518e86cab36c06600c86c10c0f0cd
Hiro-o-ai <110434009@ccalumni.meijo-u.ac.jp>
2020-08-22 23:45:38 +0900

    2.4.2

---
+++ chapter2.js
@@ -0,0 +1,40 @@
+// 演算子
+// console.log(10 + 1)
+// console.log('10' + 1)
+// var today = new Date()
+// console.log(1234 + today)
+
+// インクリメント演算子
+// var x = 3;
+// var y = x++;
+// console.log(x);
+// console.log(y);
+// var x = 3;
+// var y = ++x;
+// console.log(x);
+// console.log(y);
+
+// 代入演算子
+// var x = 1;
+// var y = x;
+// x = 2;
+// console.log(y);
+// var data1 = [0, 1 ,2];
+// var data2 = data1;
+// data1[0] = 5;
+// console.log(data2);
+// 参照型の場合はvar後に代入した値が反映される
+
+// 分割代入
+// let data = [56, 40, 23, 53, 566, 63, 1, 6];
+// // let [x0, x1, x2, x3, x4, x5, x6, x7] = data
+// // console.log(x0);
+// // console.log(x4);
+// // console.log(x7);
+// let [x0, x1, x2, x3, ...other] = data
+// console.log(x0);
+// console.log(x3);
+// console.log(other);
+
+
+
