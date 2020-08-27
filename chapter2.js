031d4721bf5f6743b19d663f03814e86984d33c4
Hiro-o-ai <110434009@ccalumni.meijo-u.ac.jp>
2020-08-27 23:54:31 +0900

    break/continue

--- chapter2.js
+++ chapter2.js
@@ -157,8 +157,19 @@
 //   console.log(value);
 // }
 
-
-
+// break/continue
+// var result = 0;
+// for (var i = 1; i <= 100; i++) {
+//   result += i;
+//   if (result > 1000) { break; }
+// }
+// console.log('合計値が1000を超えるのは' + i);
+// var result = 0;
+// for (var i = 1; i < 100; i++) {
+//   if (i % 2 === 0) { continue ; }
+//   result += i;
+// }
+// console.log('合計:' + result );
 
 
 
