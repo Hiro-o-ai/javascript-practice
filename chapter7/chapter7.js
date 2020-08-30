1cfaceb9cb7d22672a96ad2c44232171f7bcf44c
Hiro-o-ai <110434009@ccalumni.meijo-u.ac.jp>
2020-08-30 17:41:58 +0900

    タイマー機能

--- chapter7/chapter7.js
+++ chapter7/chapter7.js
@@ -1,7 +1,24 @@
-document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('fm').addEventListener('submit', function(e) {
-    if (!window.confirm('ページを送信してもいいですか？')) {
-      e.preventDefault();
-    }
-  }, false)
-}, false);
+// 確認ダイアログ
+// document.addEventListener('DOMContentLoaded', function() {
+//   document.getElementById('fm').addEventListener('submit', function(e) {
+//     if (!window.confirm('ページを送信してもいいですか？')) {
+//       e.preventDefault();
+//     }
+//   }, false)
+// }, false);
+
+// タイマー機能
+// document.addEventListener('DOMContentLoaded', function() {
+//   var timer = window.setInterval(
+//     function() {
+//       var dat = new Date();
+//       document.getElementById('result').textContent = dat.toLocaleTimeString();
+//     }, 5000);
+
+//   document.getElementById('btn').addEventListener('click', function() {
+//     window.clearInterval(timer);
+//   }, false);
+// }, false);
+
+
+
