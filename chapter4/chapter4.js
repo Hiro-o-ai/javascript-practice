c896d957d4cc0773bd48fa9cab068234322d2ce6
Hiro-o-ai <110434009@ccalumni.meijo-u.ac.jp>
2020-09-03 17:28:19 +0900

    引数の記法

--- chapter4/chapter4.js
+++ chapter4/chapter4.js
@@ -143,6 +143,117 @@
 // showMessage('山田', '田中');山田
 // 引数の数でエラーが起きない
 
+// 引数が異なるときにエラー
+// function showMessage(value) {
+//   if (arguments.length !== 1) {
+//     throw new Error('引数の数が間違っています:' + arguments.length);
+//   }
+//   console.log(value);
+// }
+// try {
+//   showMessage('山田', '田中');
+// } catch(e) {
+//   window.alert(e.message);
+// }
+
+// 引数のデフォルト値を設定
+// function getTriangle(base, height) {
+//   if (base === undefined) { base = 1; }
+//   if (height === undefined) { height = 1; }
+//   return base * height / 2;
+// }
+// console.log(getTriangle(5))
+
+// 可変長引数
+// function sum(){
+//   var result = 0;
+//   for (var i = 0, len = arguments.length; i < len; i++) {
+//     var tmp = arguments[i];
+//     if (typeof tmp !== 'number') {
+//       throw new Error('引数が数値ではありません:' + tmp);
+//     }
+//     result += tmp;
+//   }
+//   return result;
+// }
+
+// try {
+//   console.log(sum(1,3,5,7,9));
+// } catch(e) {
+//   window.alert(e.message);
+// }
+
+// 名前付き引数
+// function getTriangle(args) {
+//   if (args.base === undefined) {args.base = 1;}
+//   if (args.height === undefined) {args.height = 1;}
+//   return args.base * args.height / 2;
+// }
+// console.log(getTriangle({ base:5, height:4}));
+
+// 引数の記法
+// デフォルト値の設定
+// function getTriangle(base = 1, height = 1) {
+//   return base * height / 2;
+// }
+// console.log(getTriangle(4));
+
+// function multi(a, b = a) {
+//   return a * b;
+// }
+// console.log(multi(10,5))
+// console.log(multi(10))
+
+// デフォルトエラー
+// function required() {
+//   throw new Error('引数が不足しています');
+// }
+// function hoge(value = required()) {
+//   return value;
+// }
+// hoge();
+
+// 可変長引数の別記法
+// function sum(...nums) {
+//   let result = 0;
+//   for (let num of nums) {
+//     if (typeof num !== 'number') {
+//       throw new Error('指定値が数値ではありません:' + num);
+//     }
+//     result += num;
+//   }
+//   return result;
+// }
+// console.log(sum(1,3,5,7))
+
+// console.log(Math.max(15, -3, 78, 1))     78
+// console.log(Math.max([15, -3, 78, 1]))   NaN
+// console.log(Math.max.apply(null, [15, -3, 78, 1])) 78
+// console.log(Math.max(...[15, -3, 78, 1]))   78
+
+// 特定のプロパティを取り出す
+// function show({name}) {
+//   console.log(name);
+// };
+
+// let member = {
+//   mid: 'Y0001',
+//   name: '山田たろう',
+//   address: 't_yamada@example.com'
+// };
+
+// show(member)  山田たろう
+
+
+
+
+
+
+
+
+
+
+
 
 
 
